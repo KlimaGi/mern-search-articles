9e0e7199f5271591b99560f04c8cadfb007b4ea1
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-03 14:06:49 +0300

    env

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
