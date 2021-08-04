f7eaa52ad869ac3a544292aca167da6f3bab8c8f
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-04 19:58:10 +0300

    changes in searchIn.js

--- src/components/SearchIn.js
+++ src/components/SearchIn.js
@@ -4,11 +4,7 @@ class SearchIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchIn: [
-        { key: "Title", name: "title" },
-        { key: "Description", name: "description" },
-        { key: "Content", name: "content" },
-      ],
+      searchIn: [{ key: "Title" }, { key: "Description" }, { key: "Content" }],
     };
     this.onClickSearchIn = this.onClickSearchIn.bind(this);
   }
@@ -16,13 +12,13 @@ class SearchIn extends React.Component {
   onClickSearchIn = (event) => {
     const chosenName = this.state.searchIn.filter((el) => {
       if (event.target.value === el.key) {
-        console.log("searchIn.name", el.name);
-        return el.name;
+        // console.log("searchIn.name", el.key);
+        return el.key;
       }
     });
 
-    this.props.onClickSearchInSet(chosenName[0].name);
-    console.log("chosenName", chosenName[0].name);
+    this.props.onClickSearchInSet(chosenName[0].key.toLowerCase());
+    console.log("chosenName", chosenName[0].key.toLowerCase());
   };
   render() {
     return (
