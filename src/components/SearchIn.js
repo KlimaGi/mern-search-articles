17af543558c845eba1f9eab064b406de3ca4dc90
Giedre <giedre.klima@gmail.com>
2022-04-11 19:23:35 +0300

    searchIn.js -> class to func

--- src/components/SearchIn.js
+++ src/components/SearchIn.js
@@ -1,38 +1,28 @@
 import React from "react";
 
-export class SearchIn extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchIn: [{ key: "Title" }, { key: "Description" }, { key: "Content" }],
-    };
-    this.onClickSearchIn = this.onClickSearchIn.bind(this);
-  }
+export const SearchIn = ({ onClickSearchInSet }) => {
+  const searchIn = [
+    { key: "Title" },
+    { key: "Description" },
+    { key: "Content" },
+  ];
 
-  onClickSearchIn = (event) => {
-    const chosenName = this.state.searchIn.filter((el) => {
-      if (event.target.value === el.key) {
-        // console.log("searchIn.name", el.key);
-        return el.key;
-      }
-    });
-
-    this.props.onClickSearchInSet(chosenName[0].key.toLowerCase());
-    console.log("chosenName", chosenName[0].key.toLowerCase());
+  const onClickSearchIn = (event) => {
+    const chosenName = searchIn.find((el) => event.target.value === el.key).key;
+    onClickSearchInSet(chosenName.toLowerCase());
   };
-  render() {
-    return (
-      <div>
-        <select
-          className="form-select text-color mx-2 input-back"
-          onChange={this.onClickSearchIn}
-        >
-          <option className="text-color">Choose search in </option>
-          {this.state.searchIn.map((choosen) => {
-            return <option key={choosen.key}>{choosen.key}</option>;
-          })}
-        </select>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      <select
+        className="form-select text-color mx-2 input-back"
+        onChange={onClickSearchIn}
+      >
+        <option className="text-color">Choose search in </option>
+        {searchIn.map((choosen) => {
+          return <option key={choosen.key}>{choosen.key}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
