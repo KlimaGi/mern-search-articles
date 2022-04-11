beb2eca7c3415509dceec025c3958de7c8a91c8f
Giedre <giedre.klima@gmail.com>
2022-04-11 19:32:21 +0300

    time.js -> class to func

--- src/components/Time.js
+++ src/components/Time.js
@@ -4,33 +4,25 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-daterangepicker/daterangepicker.css";
 import moment from "moment";
 
-export class Time extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleCallback = this.handleCallback.bind(this);
-  }
-
-  handleCallback(start, end) {
+export const Time = ({ onSetTime }) => {
+  const handleCallback = (start, end) => {
     const startTime = start._d.toISOString().split(".")[0] + "Z";
     const endTime = end._d.toISOString().split(".")[0] + "Z";
     console.log(start._d.toISOString());
-    this.props.onSetTime(startTime, endTime);
-  }
+    onSetTime(startTime, endTime);
+  };
 
-  render() {
-    return (
-      <DateRangePicker
-        initialSettings={{
-          startDate: moment().subtract(2, "days"),
-          endDate: moment(),
-        }}
-        onCallback={this.handleCallback}
-      >
-        <button type="button" className="btn btn-outline-light mx-2 px-3">
-          Select date
-        </button>
-      </DateRangePicker>
-    );
-  }
-}
+  return (
+    <DateRangePicker
+      initialSettings={{
+        startDate: moment().subtract(2, "days"),
+        endDate: moment(),
+      }}
+      onCallback={handleCallback}
+    >
+      <button type="button" className="btn btn-outline-light mx-2 px-3">
+        Select date
+      </button>
+    </DateRangePicker>
+  );
+};
